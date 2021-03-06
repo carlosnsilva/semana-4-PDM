b9762d44ce8e4f01fe0d946447d1f9b30c8ce27e
carlosnsilva <cmanoel17@gmail.com>
2021-03-06 16:31:31 -0300

    Subindo atividade finalizada

---
+++ README.md
@@ -0,0 +1,2 @@
+## Repositorio contendo a atividade da semana 4 da disciplina de PDM
+### Discente: Carlos Manoel Nunes
--- app/src/main/java/com/example/semana4/SegundaTela.kt
+++ app/src/main/java/com/example/semana4/SegundaTela.kt
@@ -2,17 +2,25 @@ package com.example.semana4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import com.example.semana4.Pessoa
 
 class SegundaTela : AppCompatActivity() {
 
     private lateinit var pessoa: Pessoa
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_segunda_tela)
 
-        this.pessoa = (Pessoa) getIntent.getSerializable("objetoPessoa")
+        this.pessoa = getIntent().getExtras()!!.getSerializable("objetoPessoa") as Pessoa
+        this.tvResult = findViewById(R.id.resultado)
+
+        tvResult.setText(pessoa.nome + ", você possui " + pessoa.idade().toString() + " anos!")
+
+
+
     }
 
 
